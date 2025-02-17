c76128d56388dfec29452d5450908b96e5acc01f
savchenko-maksym <maxim.savchenko91@gmail.com>
2025-02-18 01:10:52 +0200

    add settings

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// Список параметрів рядка запиту, які тобі обов'язково необхідно вказати:
+
+// key — твій унікальний ключ доступу до API.
+// q — слово для пошуку. Те, що буде вводити користувач.
+// image_type — тип зображення. Потрібні тільки фотографії, тому постав значення photo.
+// orientation — орієнтація фотографії. Постав значення horizontal.
+// safesearch — фільтр за віком. Постав значення true.
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
